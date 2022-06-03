b64b48a4cdc143f68a2f8aee986bab2179daf633
Đỗ Việt Hoàng <hoangdv2429@gmail.com>
2022-06-03 13:21:31 +0700

    add new Todo

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 /* 
   【inputコンポーネント】
 　・新しいTodoを作成するINPUTフィールドを作成するコンポーネント
 　・Enterをクリックされたら入力された文字を使って新しいTodoを作成する
 */
-function Input(  ) {
+
+  function Input( {onSubmit} ) {
+    const handleEnter = (e)=>{
+      if(e.key==='Enter'){
+        onSubmit(e);
+      }
+    }
 
   return (
     <div className="panel-block">
+      <input class="input" type="text" placeholder="Todo を入力してください"　onKeyDown={handleEnter}></input>
     </div>
   );
 }
