fbdedd6e782e2a4b6fd292eb460a8371ae2185e2
Đỗ Việt Hoàng <hoangdv2429@gmail.com>
2022-06-08 22:20:46 +0700

    Add Tabs

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -16,21 +16,33 @@ import Filter from './Filter';
 import useStorage from '../hooks/storage';
 
 /* ライブラリ */
-import {getKey} from "../lib/util";
+import { getKey } from "../lib/util";
 
 function Todo() {
   const [items, putItems] = useState([
-      /* テストコード 開始 */
+    /* テストコード 開始 */
     { key: getKey(), text: '日本語の宿題', done: false },
     { key: getKey(), text: 'reactを勉強する', done: false },
     { key: getKey(), text: '明日の準備をする', done: false },
     /* テストコード 終了 */
   ]);
 
+  const [filter, setFilter] = React.useState('ALL');
+
+  const displayItems = items.filter(item => {
+    if (filter === 'ALL') return true;
+    if (filter === 'TODO') return !item.done;
+    if (filter === 'DONE') return item.done;
+  });
+
+  const handleFilterChange = (value) => {
+    setFilter(value);
+  }
+
   const handleCheck = (changedItem, checked) => {
     const newItem = items.map(item => {
       if (item.key === changedItem.key) {
-        item.done = checked;
+        item.done = !item.done;
       }
       return item;
     });
@@ -40,9 +52,10 @@ function Todo() {
   const handleEnter = (event) => {
     putItems(items => [
       ...items,
-      {key:getKey(), text:event.target.value, done:false}
+      { key: getKey(), text: event.target.value, done: false }
     ])
   }
+
 
   return (
     <div className="panel">
@@ -50,8 +63,9 @@ function Todo() {
         ITSS ToDoアプリ
       </div>
       <Input onSubmit={handleEnter}></Input>
-      {items.map(item => (
-        <TodoItem key={item.key} item={item} onCheck={handleCheck}/>
+      <Filter onChange={handleFilterChange} value={filter} />
+      {displayItems.map(item => (
+        <TodoItem key={item.key} item={item} onCheck={handleCheck} />
       ))}
       <div className="panel-block">
         {items.length} items
